ce8343911815ee3f4fa9c76545c6745324d0f516
Georgee-lee <rjsahdia3@gmail.com>
2019-08-18 14:58:33 +0900

    Clarifiy 라이브러리 적용

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import Navigation from "./Components/Navigation";
 import Logo from "./Components/Logo";
 import Rank from "./Components/Rank";
 import ImageLinkForm from "./Components/ImageLinkForm";
+import FaceRecognition from "./Components/FaceRecognition";
+
+import Clarifai from "clarifai";
+
+const app = new Clarifai.App({
+  apiKey: "e71ae76c29954fd190367b5ad40ea205"
+});
 
 const particlesOptions = {
   particles: {
@@ -19,17 +26,50 @@ const particlesOptions = {
   }
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Particles params={particlesOptions} className="particles" />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: ""
+    };
+  }
+
+  inputChangeHandler = e => {
+    this.setState({
+      input: e.target.value
+    });
+  };
+
+  submitHandler = e => {
+    this.setState({
+      imageUrl: this.state.input
+    });
+
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", this.state.input).then(
+      function(response) {
+        // do something with responseconsole.log(response);
+        console.log(response.outputs[0].data.regions[0]["region_info"]["bounding_box"]);
+      },
+      function(err) {
+        // there was an error}
+        throw new Error(err);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Particles params={particlesOptions} className="particles" />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.inputChangeHandler} onSubmit={this.submitHandler} />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
