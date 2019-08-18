57eb13a836f56f783521e4d5a5f481f4ed865539
Georgee-lee <rjsahdia3@gmail.com>
2019-08-18 14:19:04 +0900

    add particls library

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from "react";
 import "./App.css";
 
+import Particles from "react-particles-js";
 import Navigation from "./Components/Navigation";
 import Logo from "./Components/Logo";
+import Rank from "./Components/Rank";
 import ImageLinkForm from "./Components/ImageLinkForm";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 80,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles params={particlesOptions} className="particles" />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
